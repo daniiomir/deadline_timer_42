707ac9ef72333afcdccfc6d50add9b2beee15abb
swarner <daniiomir@yandex.ru>
2019-05-16 16:25:35 +0300

    Deadline will work only on MAC OS in school 42

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cp -f ./dist/deadline ~/
-echo "alias deadline=\"~/deadline\"" >> ~/.zshrc
-sleep 1
-echo "Finished! Restart your terminal."
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	cp -f ./dist/deadline_mac ~/
+	echo "alias deadline=\"~/deadline_mac\"" >> ~/.zshrc
+	sleep 1
+	echo "Finished! Restart your terminal."
+else
+	echo "Error! Program will work only on school 42 MAC OS."
+fi
