a2c85e908dced3fbc1d64feb9546eb04109975a9
swarner <daniiomir@yandex.ru>
2019-10-25 17:12:09 +0300

    Corrections

--- main.py
+++ main.py
@@ -14,25 +14,28 @@ lvl_21 = datetime.datetime(2023, 3, 31)
 def level(level):
     now = datetime.datetime.now()
     rd = rdelta.relativedelta(level, now)
-    print("\t *** Your deadline will be in {0.months} months {0.days} days {0.hours} hours ***".format(rd))
+    if rd.hours < 0:
+        print("\t *** You fucked up! Say goodbye to School 21! :( ***")
+    else:
+        print("\t *** Your deadline will be in {0.months} months {0.days} days {0.hours} hours ***".format(rd))
 
 def main():
     if len(sys.argv) == 2:
-        if sys.argv[1] == '0':
+        if int(sys.argv[1]) == '0':
             level(lvl_1)
-        elif sys.argv[1] == '1':
+        elif int(sys.argv[1]) < 2:
             level(lvl_2)
-        elif sys.argv[1] == '2' or sys.argv[1] == '3':
+        elif int(sys.argv[1]) < 4:
             level(lvl_4)
-        elif sys.argv[1] >= '4' and sys.argv[1] < '5':
+        elif int(sys.argv[1]) < 5:
             level(lvl_5)
-        elif sys.argv[1] >= '5' and sys.argv[1] < '7':
+        elif int(sys.argv[1]) < 7:
             level(lvl_7)
-        elif sys.argv[1] >= '7' and sys.argv[1] < '14':
+        elif int(sys.argv[1]) < 14:
             level(lvl_14)
-        elif sys.argv[1] >= '14' and sys.argv[1] < '16':
+        elif int(sys.argv[1]) < 16:
             level(lvl_16)
-        elif sys.argv[1] >= '16' and sys.argv[1] < '21':
+        elif int(sys.argv[1]) < 21:
             level(lvl_21)
         else:
             print("usage: deadline [your_level]")
